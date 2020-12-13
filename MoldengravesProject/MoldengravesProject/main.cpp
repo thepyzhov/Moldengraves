c4921c1b086f788cd06fcb2b11e92e4bf46502cd
Dmitry Pyzhov <fiche40@gmail.com>
2020-12-13 07:50:55 +0300

    Core Refactoring & Movement FIX

--- MoldengravesProject/MoldengravesProject/main.cpp
+++ MoldengravesProject/MoldengravesProject/main.cpp
@@ -9,6 +9,7 @@
 // Size of the world map
 const int mapWidth = 24;
 const int mapHeight = 24;
+const float fpsRefreshTime = 0.1f;
 
 int worldMap[mapWidth][mapHeight] =
 {
@@ -38,36 +39,27 @@ int worldMap[mapWidth][mapHeight] =
   {1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1}
 };
 
-int SCREEN_WIDTH = 1440;
-int SCREEN_HEIGHT = 900;
+int screenWidth = 1440;
+int screenHeight = 900;
 
 int main(int, char const**)
 {
     sf::RenderWindow window(sf::VideoMode(1440, 900), "Moldengraves");
+    window.setFramerateLimit(60);
     
-    sf::VertexArray lines(sf::Lines, 18 * SCREEN_WIDTH);
+    sf::VertexArray lines(sf::Lines, 18 * screenWidth);
+    
     // Player
-    int posX = 22;
-    int posY = 12;
-    
-    int dirX = -1;
-    int dirY = 0;
+    sf::Vector2f position(22, 12);
+    sf::Vector2f direction(-1, 0);
     
     // Camera
-    double planeX = 0;
-    double planeY = 0.66;
+    sf::Vector2f plane(0, 0.66);
     
     sf::Clock clock;
-    double time = 0; //time of current frame
-    double oldTime = 0; //time of previous frame
-    
-    oldTime = time;
-    time = clock.getElapsedTime().asMicroseconds();
-    double frameTime = (time - oldTime) / 1000.0; //frameTime is the time this frame has taken, in seconds
-
-    //speed modifiers
-    double moveSpeed = frameTime * 5.0; //the constant value is in squares/second
-    double rotSpeed = frameTime * 3.0; //the constant value is in radians/second
+    float dtCounter = 0.0f;
+    int frameCounter = 0;
+    int64_t frameTimeMicro = 0;
 
     sf::Image icon;
     if (!icon.loadFromFile(resourcePath() + "icon.png")) {
@@ -78,6 +70,23 @@ int main(int, char const**)
     // Start the game loop
     while (window.isOpen())
     {
+        float dt = clock.restart().asSeconds();
+        if (dtCounter >= fpsRefreshTime) {
+            float fps = (float) frameCounter / dtCounter;
+            frameTimeMicro /= frameCounter;
+            // Text output can be placed here
+            std::cout << "FPS: " << fps << std::endl;
+            dtCounter = 0.0f;
+            frameCounter = 0;
+            frameTimeMicro = 0;
+        }
+        dtCounter += dt;
+        ++frameCounter;
+        
+        //speed modifiers
+        double moveSpeed = 5.0 * dt; //the constant value is in squares/second
+        double rotSpeed = 3.0 * dt; //the constant value is in radians/second
+        
         // Process events
         sf::Event event;
         while (window.pollEvent(event))
@@ -98,35 +107,52 @@ int main(int, char const**)
 //            }
             
             if (sf::Keyboard::isKeyPressed(sf::Keyboard::W)) {
-                if (worldMap[int(posX + dirX * moveSpeed)][int(posY)] == false) posX += dirX * moveSpeed;
-                if (worldMap[int(posX)][int(posY + dirY * moveSpeed)] == false) posY += dirY * moveSpeed;
+                if (worldMap[int(position.x + direction.x * moveSpeed)][int(position.y)] == false) position.x += direction.x * moveSpeed;
+                if (worldMap[int(position.x)][int(position.y + direction.y * moveSpeed)] == false) position.y += direction.y * moveSpeed;
             }
 
             if (sf::Keyboard::isKeyPressed(sf::Keyboard::S)) {
-                if (worldMap[int(posX - dirX * moveSpeed)][int(posY)] == false) posX -= dirX * moveSpeed;
-                if (worldMap[int(posX)][int(posY - dirY * moveSpeed)] == false) posY -= dirY * moveSpeed;
+                if (worldMap[int(position.x - direction.x * moveSpeed)][int(position.y)] == false) position.x -= direction.x * moveSpeed;
+                if (worldMap[int(position.x)][int(position.y - direction.y * moveSpeed)] == false) position.y -= direction.y * moveSpeed;
+            }
+            
+            if (sf::Keyboard::isKeyPressed(sf::Keyboard::A)) {
+                float oldDirX = direction.x;
+                direction.x = direction.x * cos(rotSpeed) - direction.y * sin(rotSpeed);
+                direction.y = oldDirX * sin(rotSpeed) + direction.y * cos(rotSpeed);
+                
+                float oldPlaneX = plane.x;
+                plane.x = plane.x * cos(rotSpeed) - plane.y * sin(rotSpeed);
+                plane.y = oldPlaneX * sin(rotSpeed) + plane.y * cos(rotSpeed);
+            }
+
+            if (sf::Keyboard::isKeyPressed(sf::Keyboard::D)) {
+                float oldDirX = direction.x;
+                direction.x = direction.x * cos(-rotSpeed) - direction.y * sin(-rotSpeed);
+                direction.y = oldDirX * sin(-rotSpeed) + direction.y * cos(-rotSpeed);
+                
+                float oldPlaneX = plane.x;
+                plane.x = plane.x * cos(-rotSpeed) - plane.y * sin(-rotSpeed);
+                plane.y = oldPlaneX * sin(-rotSpeed) + plane.y * cos(-rotSpeed);
             }
         }
-
-        // Clear screen
-        window.clear();
         
-        for (int x = 0; x < SCREEN_WIDTH; x++) {
-            double cameraX = 2 * x / (double) SCREEN_WIDTH - 1;
-            double rayDirX = dirX + planeX * cameraX;
-            double rayDirY = dirY + planeY * cameraX;
+        lines.resize(0);
+        
+        for (int x = 0; x < screenWidth; x++) {
             
-            int mapX = (int) posX;
-            int mapY = (int) posY;
+            float cameraX = 2 * x / (double) screenWidth - 1;
+            sf::Vector2f rayPos = position;
+            sf::Vector2f rayDir = direction + plane * cameraX;
             
-            double sideDistX;
-            double sideDistY;
+            sf::Vector2i mapPos(rayPos);
+            sf::Vector2f sideDist;
+            sf::Vector2f deltaDist(
+                       sqrt(1.0f + pow(rayDir.y, 2) / pow(rayDir.x, 2)),
+                       sqrt(1.0f + pow(rayDir.x, 2) / pow(rayDir.y, 2))
+            );
             
-//            double deltaDistX = std::abs(1 / rayDirX);
-//            double deltaDistY = std::abs(1 / rayDirY);
-            double deltaDistX = (rayDirY == 0) ? 0 : ((rayDirX == 0) ? 1 : std::abs(1 / rayDirX));
-            double deltaDistY = (rayDirX == 0) ? 0 : ((rayDirY == 0) ? 1 : std::abs(1 / rayDirY));
-            double perpWallDist;
+            float perpWallDist;
             
             int stepX;
             int stepY;
@@ -134,74 +160,64 @@ int main(int, char const**)
             int hit = 0;
             int side;
             
-            if (rayDirX < 0) {
+            if (rayDir.x < 0) {
                 stepX = -1;
-                sideDistX = (posX - mapX) * deltaDistX;
+                sideDist.x = (position.x - mapPos.x) * deltaDist.x;
             } else {
                 stepX = 1;
-                sideDistX = (mapX + 1.0 - posX) * deltaDistX;
+                sideDist.x = (mapPos.x + 1.0 - position.x) * deltaDist.x;
             }
             
-            if (rayDirY < 0) {
+            if (rayDir.y < 0) {
                 stepY = -1;
-                sideDistY = (posY - mapY) * deltaDistY;
+                sideDist.y = (position.y - mapPos.y) * deltaDist.y;
             } else {
                 stepY = 1;
-                sideDistY = (mapY + 1.0 - posY) * deltaDistY;
+                sideDist.y = (mapPos.y + 1.0 - position.y) * deltaDist.y;
             }
             
             // DDA
             while (hit == 0) {
-                if (sideDistX < sideDistY) {
-                    sideDistX += deltaDistX;
-                    mapX += stepX;
+                if (sideDist.x < sideDist.y) {
+                    sideDist.x += deltaDist.x;
+                    mapPos.x += stepX;
                     side = 0;
                 } else {
-                    sideDistY += deltaDistY;
-                    mapY += stepY;
+                    sideDist.y += deltaDist.y;
+                    mapPos.y += stepY;
                     side = 1;
                 }
-                if (worldMap[mapX][mapY] > 0) {
+                if (worldMap[mapPos.x][mapPos.y] > 0) {
                     hit = 1;
                 }
             }
             
             if (side == 0) {
-                perpWallDist = (mapX - posX + (1- stepX) / 2) / rayDirX;
+                perpWallDist = (mapPos.x - position.x + (1- stepX) / 2) / rayDir.x;
             } else {
-                perpWallDist = (mapY - posY + (1- stepY) / 2) / rayDirY;
+                perpWallDist = (mapPos.y - position.y + (1- stepY) / 2) / rayDir.y;
             }
             int h = 400;
             
-            int lineHeight = (int) (SCREEN_HEIGHT / perpWallDist);
+            int lineHeight = (int) (screenHeight / perpWallDist);
             
-            int drawStart = -lineHeight / 2 + SCREEN_HEIGHT / 2;
+            int drawStart = -lineHeight / 2 + screenHeight / 2;
             if (drawStart < 0) {
                 drawStart = 0;
             }
             
-            int drawEnd = lineHeight / 2 + SCREEN_HEIGHT / 2;
-            if (drawEnd >= SCREEN_HEIGHT) {
-                drawEnd = SCREEN_HEIGHT - 1;
+            int drawEnd = lineHeight / 2 + screenHeight / 2;
+            if (drawEnd >= screenHeight) {
+                drawEnd = screenHeight - 1;
             }
             
             sf::Color lineColor;
-            switch (worldMap[mapX][mapY]) {
-                case 1:
-                    lineColor = sf::Color::Red;
-                    break;
-                case 2:
-                    lineColor = sf::Color::Green;
-                    break;
-                case 3:
-                    lineColor = sf::Color::Blue;
-                    break;
-                case 4:
-                    lineColor = sf::Color::White;
-                    break;
-                default:
-                    lineColor = sf::Color::Yellow;
-                    break;
+            switch (worldMap[mapPos.x][mapPos.y]) {
+                case 1: lineColor = sf::Color::Red; break;
+                case 2: lineColor = sf::Color::Green; break;
+                case 3: lineColor = sf::Color::Blue; break;
+                case 4: lineColor = sf::Color::White; break;
+                default: lineColor = sf::Color::Yellow; break;
             }
             
             if (side == 1) {
@@ -210,13 +226,13 @@ int main(int, char const**)
                 lineColor.b /= 2;
             }
             
-            sf::RectangleShape line(sf::Vector2f(1.f, lineHeight));
-            line.setPosition(x, drawStart);
-            line.setFillColor(lineColor);
-            window.draw(line);
+            lines.append(sf::Vertex(sf::Vector2f((float) x, (float) drawStart), lineColor));
+            lines.append(sf::Vertex(sf::Vector2f((float) x, (float) drawEnd), lineColor));
         }
-    
-        // Update the window
+        
+        window.clear();
+        window.draw(lines);
+        frameTimeMicro = clock.getElapsedTime().asMicroseconds();
         window.display();
     }
 
